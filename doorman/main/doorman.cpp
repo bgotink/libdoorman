1258d9ee24e36bba2e461de7cff1f5a2e17b5ad7
Bram Vercruysse <bram123@gmail.com>
2015-10-28 18:39:04 +0100

    Fixes for the size of an array.

--- doorman/main/doorman.cpp
+++ doorman/main/doorman.cpp
@@ -82,14 +82,14 @@ int do_write(char *channel_str, char *sound_str) {
   int channel = atoi(channel_str);
   int sound = atoi(sound_str);
   static int channel_map[] = {0b1111,0b0011,0b1011,0b0000,0b1000,0b0100,0b1100,0b0101,0b1101,0b0001,0b1001,0b0010,0b1010,0b0110,0b1110,0b0111};
-  if(channel > channel_map.size() || channel < 1){
+  if(channel > sizeof(channel_map) || channel < 1){
     cerr << "Channel not existing" << newline
     return 1
   }
   int channel_bit = channel_map[channel - 1];
 
   static int sound_map[] = {0b11110111,0b11111011,0b11111101};
-  if(sound > sound_map.size() || channel < 1){
+  if(sound > sizeof(sound_map) || channel < 1){
     cerr << "Channel not existing" << newline
     return 1
   }
@@ -139,6 +139,6 @@ int main(int argc, char ** argv) {
   } else if (command_write == argv[1] && argc == 4) {
     return do_write(argv[2], argv[3]);
   } else {
-    fprintf(stderr, "Invalid mode \"%s\", valid modes are \"read\", \"read-human\" and \"write <channel> <sound>\"\n\nChannel can range from 1 to %d\nSound can range from 1 to %d\n", argv[1], channel_map.size(), sound_map.size());
+    fprintf(stderr, "Invalid mode \"%s\", valid modes are \"read\", \"read-human\" and \"write <channel> <sound>\"\n\nChannel can range from 1 to %d\nSound can range from 1 to %d\n", argv[1], sizeof(channel_map), sizeof(sound_map));
   }
 }
